c3f87e65d7f45bf1c13a2b7b6d58d6cfd3192c12
Maciej Siwek <ms223iu@student.lnu.se>
2018-03-23 05:10:56 +0100

    Properly formatted

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
   <footer class="footer">
-      <div class="container">
-        <div class="content has-text-centered">
-          <p>Developed by <a class="has-text-primary" href="https://github.com/ms223iu" target="_blank">Maciej Siwek</a></p>
-        </div>
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>Developed by
+          <a class="has-text-primary" href="https://github.com/ms223iu" target="_blank">Maciej Siwek</a>
+        </p>
       </div>
-    </footer>
+    </div>
+  </footer>
 </template>

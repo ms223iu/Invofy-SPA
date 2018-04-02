39cb07fc6a7f9222626c4c80c4248537342700d7
Maciej Siwek <ms223iu@student.lnu.se>
2018-04-02 02:39:15 +0200

    Addresses can now be edited

---
+++ src/components/address/AddressEntry.vue
@@ -0,0 +1,178 @@
+<template>
+  <b-collapse class="card" :open="false">
+    <div slot="trigger" slot-scope="props" class="card-header">
+      <div class="card-header-title">{{ address.displayName }}</div>
+      <a class="card-header-icon">
+        <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+      </a>
+    </div>
+    <div class="card-content">
+      <div v-if="isEditing">
+        <div>
+          <div class="field">
+            <label class="label">Visningsnamn</label>
+            <p :class="{ 'control': true }">
+              <input v-model="address.displayName" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('visningsnamn')}" name="visningsnamn" type="text" placeholder="visningsnamn" :disabled="isLoading">
+              <span v-show="errors.has('visningsnamn')" class="help is-danger">{{ errors.first('visningsnamn') }}</span>
+            </p>
+          </div>
+
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <label class="label">Namn</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.customer" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('namn')}" name="namn" type="text" placeholder="namn" :disabled="isLoading">
+                  <span v-show="errors.has('namn')" class="help is-danger">{{ errors.first('namn') }}</span>
+                </p>
+              </div>
+
+              <div class="field">
+                <label class="label">Adressrad 1</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.addr1" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('adressrad1')}" name="adressrad1" type="text" placeholder="adressrad 1" :disabled="isLoading">
+                  <span v-show="errors.has('adressrad1')" class="help is-danger">{{ errors.first('adressrad1') }}</span>
+                </p>
+              </div>
+
+              <div class="field">
+                <label class="label">Adressrad 2</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.addr2" :class="{'input': true, 'is-danger': errors.has('adressrad2')}" name="adressrad2" type="text" placeholder="adressrad 2" :disabled="isLoading">
+                  <span v-show="errors.has('adressrad2')" class="help is-danger">{{ errors.first('adressrad2') }}</span>
+                </p>
+              </div>
+
+              <div class="field">
+                <label class="label">Postnummer</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.post" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('postnummer')}" name="postnummer" type="text" placeholder="postnummer" :disabled="isLoading">
+                  <span v-show="errors.has('postnummer')" class="help is-danger">{{ errors.first('postnummer') }}</span>
+                </p>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="field">
+                <label class="label">Stad</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.city" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('stad')}" name="stad" type="text" placeholder="stad" :disabled="isLoading">
+                  <span v-show="errors.has('stad')" class="help is-danger">{{ errors.first('stad') }}</span>
+                </p>
+              </div>
+
+              <div class="field">
+                <label class="label">Land</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.country" v-validate.initial="'required'" :class="{'input': true, 'is-danger': errors.has('land')}" name="land" type="text" placeholder="land" :disabled="isLoading">
+                  <span v-show="errors.has('land')" class="help is-danger">{{ errors.first('land') }}</span>
+                </p>
+              </div>
+              <div class="field">
+                <label class="label">Referensrad 1</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.ref1" :class="{'input': true, 'is-danger': errors.has('referensrad1')}" name="referensrad1" type="text" placeholder="referensrad 1" :disabled="isLoading">
+                  <span v-show="errors.has('referensrad1')" class="help is-danger">{{ errors.first('referensrad1') }}</span>
+                </p>
+              </div>
+              <div class="field">
+                <label class="label">Referensrad 2</label>
+                <p :class="{ 'control': true }">
+                  <input v-model="address.ref2" :class="{'input': true, 'is-danger': errors.has('referensrad2')}" name="referensrad2" type="text" placeholder="referensrad 2" :disabled="isLoading">
+                  <span v-show="errors.has('referensrad2')" class="help is-danger">{{ errors.first('referensrad2') }}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <p class="title has-text-centered is-size-4 mb-2">Förhandsgranskning</p>
+      </div>
+      <AddressPreview :address="address"></AddressPreview>
+    </div>
+    <footer class="card-footer">
+      <a v-if="!isEditing" class="card-footer-item has-text-info has-text-weight-semibold" @click="startEdit()">Ändra</a>
+      <a v-if="!isEditing" class="card-footer-item has-text-danger has-text-weight-bold" @click="removeAddress(address._id)">Ta bort</a>
+      <a v-if="isEditing" class="card-footer-item has-text-black has-text-weight-semibold" @click="cancelEdit()">Avbryt</a>
+      <a v-if="isEditing" class="card-footer-item has-text-success has-text-weight-bold" @click="saveAddress(address._id)">Spara</a>
+    </footer>
+  </b-collapse>
+</template>
+
+<script>
+import AddressPreview from '../address/AddressPreview';
+import { Toast } from '../../mixins/Toast';
+export default {
+  components: { AddressPreview },
+  props: ['data'],
+  mixins: [Toast],
+
+  data() {
+    return {
+      isEditing: false,
+      isLoading: false,
+      address: this.data,
+      addressBackup: null
+    };
+  },
+
+  methods: {
+    startEdit() {
+      this.isEditing = true;
+      this.addressBackup = JSON.parse(JSON.stringify(this.data));
+    },
+
+    cancelEdit() {
+      this.isEditing = false;
+      this.address = this.addressBackup;
+    },
+
+    saveAddress(id) {
+      this.$validator.validateAll().then(result => {
+        if (!result) return;
+
+        if (
+          JSON.stringify(this.address) === JSON.stringify(this.addressBackup)
+        ) {
+          this.cancelEdit();
+          return;
+        }
+
+        this.apiUpdateAddress(id);
+      });
+    },
+
+    removeAddress() {},
+
+    apiUpdateAddress(id) {
+      this.isLoading = true;
+
+      axios
+        .put('api/address/' + id, this.address)
+        .then(response => {
+          this.isEditing = false;
+          this.showSuccessToast('Ändringarna sparades', 2000);
+        })
+        .catch(err => {
+          const status = err.response.status;
+          this.address = this.addressBackup;
+
+          if (status == 409) {
+            this.showErrorToast('Det finns redan en adress med detta namnet');
+          } else {
+            this.showErrorToast('Något gick fel. Försök igen senare');
+          }
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.label {
+  color: black;
+}
+</style>
--- src/components/dashboard/AddressList.vue
+++ src/components/dashboard/AddressList.vue
@@ -1,42 +1,24 @@
 <template>
-  <div class="is-loading">
-    <b-collapse class="card" v-for="address in addresses" :key="address._id" :open="false">
-      <div slot="trigger" slot-scope="props" class="card-header">
-        <p class="card-header-title" >{{ address.name }}</p>
-          <a class="card-header-icon">
-            <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
-          </a>
-            </div>
-            <div class="card-content">
-                <div class="content has-text-centered" style="white-space: pre-line">{{ address.address }}</div>
-            </div>
-            <footer class="card-footer">
-                <a class="card-footer-item has-text-info has-text-weight-semibold" @click="editAddress(address)">Ändra</a>
-                <a class="card-footer-item has-text-danger has-text-weight-semibold" @click="removeAddress(address)">Ta bort</a>
-            </footer>
-        </b-collapse>
+  <div>
+    <AddressEntry v-for="(address, index) in addresses" :data="address" :key="index"></AddressEntry>
   </div>
 </template>
 
 <script>
+import AddressEntry from '../address/AddressEntry';
 import { Toast } from '../../mixins/Toast';
+
 export default {
+  components: { AddressEntry },
   mixins: [Toast],
   data() {
     return {
-      addresses: null,
-      currentAddress: null
+      addresses: null
     };
   },
 
   created() {
     this.apiGetAddresses();
-  },
-
-  computed: {
-    hasNoAddresses() {
-      return this.addresses && this.addresses.length <= 0;
-    }
   },
 
   methods: {
@@ -49,12 +31,6 @@ export default {
         .catch(error => {
           console.log('Catch from list: ' + error);
         });
-    },
-
-    editAddress(address) {},
-
-    removeAddress(address) {
-      //this.addresses.splice(this.addresses.indexOf(address), 1);
     }
   }
 };
@@ -62,6 +38,6 @@ export default {
 
 <style scoped>
 .card {
-  margin-bottom: 10px;
+  margin-bottom: 20px;
 }
 </style>

04af91edb025bc47aeabfa90f579ca4da0561b48
Maciej Siwek <me@maciejsiwek.com>
2018-04-17 22:53:42 +0200

    Variable change

--- src/components/company/InputForm.vue
+++ src/components/company/InputForm.vue
@@ -5,35 +5,35 @@
         <div class="field">
           <label class="label">Adressrad 1*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.addr1" v-validate="'required'" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('adressrad1')}" name="adressrad1" type="text" placeholder="adressrad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.addr1" v-validate="'required'" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('adressrad1')}" name="adressrad1" type="text" placeholder="adressrad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Adressrad 2</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.addr2" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('adressrad2')}" name="adressrad2" type="text" placeholder="adressrad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.addr2" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('adressrad2')}" name="adressrad2" type="text" placeholder="adressrad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Postnummer*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.post" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('postnummer')}" name="postnummer" type="text" placeholder="postnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.post" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('postnummer')}" name="postnummer" type="text" placeholder="postnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field ">
           <label class="label">Stad*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.city" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('stad')}" name="stad" type="text" placeholder="stad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.city" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('stad')}" name="stad" type="text" placeholder="stad" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Land*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.country" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('land')}" name="land" type="text" placeholder="land" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.country" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('land')}" name="land" type="text" placeholder="land" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
@@ -43,34 +43,34 @@
         <div class="field">
           <label class="label">Företagsnamn*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.name" v-validate="'required'" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('namn')}" name="namn" type="text" placeholder="namn" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.name" v-validate="'required'" @input="inputChanged()" :class="{'input': true, 'is-danger': errors.has('namn')}" name="namn" type="text" placeholder="namn" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Ägare*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.owner" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('ägare')}" name="ägare" type="text" placeholder="ägare" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.owner" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('ägare')}" name="ägare" type="text" placeholder="ägare" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Telefonnummer*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.phone" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('telefonnummer')}" name="telefonnummer" type="text" placeholder="telefonnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.phone" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('telefonnummer')}" name="telefonnummer" type="text" placeholder="telefonnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
 
         <div class="field">
           <label class="label">Organisationsnummer*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.org" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('organisationsnummer')}" name="organisationsnummer" type="text" placeholder="organisationsnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.org" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('organisationsnummer')}" name="organisationsnummer" type="text" placeholder="organisationsnummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
         <div class="field">
           <label class="label">Kontonummer*</label>
           <p :class="{ 'control': true }">
-            <input v-model="company.bankacc" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('kontonummer')}" name="kontonummer" type="text" placeholder="kontonummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="isLoading">
+            <input v-model="company.bankacc" @input="inputChanged()" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('kontonummer')}" name="kontonummer" type="text" placeholder="kontonummer" autocomplete="off" autocorrect="off" spellcheck="false" :readonly="readOnly">
           </p>
         </div>
       </div>
@@ -83,7 +83,7 @@ import { EventBus } from '../../event-bus';
 import { Focus } from '../../mixins/Focus';
 
 export default {
-  props: ['isLoading', 'data'],
+  props: ['readOnly', 'data'],
   mixins: [Focus],
   data() {
     return {

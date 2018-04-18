df10655d579fa690a2bc8eb64edc145d92d3d60a
Maciej Siwek <me@maciejsiwek.com>
2018-04-18 19:58:40 +0200

    Company information can now be managed

--- src/views/dashboard/MyCompany.vue
+++ src/views/dashboard/MyCompany.vue
@@ -1,40 +1,89 @@
 <template>
   <div>
-    <SectionInfo>Företagsinformation som du ser nedan kommer att finnas automatiskt på alla dina nya fakturor. Vid informationsändring förändras inte infon på befintliga fakturor.</SectionInfo>
-    <InputForm :readOnly="!isEditing"></InputForm>
+    <SectionInfo>Företagsinformation som du ser nedan kommer att automatiskt finnas på alla nya fakturor. Vid informationsändring förändras inte infon på befintliga fakturor.</SectionInfo>
+    <InputForm :readOnly="!isEditing" :company="company" @response="response"></InputForm>
 
-    <button v-if="!isEditing" class="button is-danger is-medium is-fullwidth mt-2" @click="edit">Ändra</button>
-    <button v-else class="button is-success is-medium is-fullwidth mt-2" @click="save">Spara ändringar</button>
+    <button v-if="!isEditing" class="button is-danger is-medium is-fullwidth mt-1" @click="edit">Ändra</button>
+
+    <div v-else class="is-pulled-right mt-1">
+      <button class="button is-info is-medium" @click="cancel">Avbryt</button>
+      <button class="button is-success is-medium" @click="save">Spara ändringar</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { EventBus } from '../../event-bus';
 import { Toast } from '../../mixins/Toast';
+import { ObjectUtil } from '../../mixins/ObjectUtil';
 import SectionInfo from '../../components/dashboard/SectionInfo';
 import InputForm from '../../components/company/InputForm';
 
 export default {
   components: { SectionInfo, InputForm },
-  mixins: [Toast],
+  mixins: [Toast, ObjectUtil],
   data() {
     return {
+      company: {},
+      companyOld: {},
       isEditing: false
     };
+  },
+
+  created() {
+    this.apiGetCompanyInfo();
   },
 
   methods: {
     edit() {
       this.isEditing = true;
+      this.companyOld = JSON.parse(JSON.stringify(this.company));
     },
 
     save() {
-      this.isEditing = false;
-      this.showSuccessToast('Ändringarna sparades', 1500);
+      EventBus.$emit('COMPANY_INPUT_VALIDATE');
     },
 
-    apiGetCompanyInfo() {},
+    cancel() {
+      this.isEditing = false;
+      this.company = this.companyOld;
+      this.companyOld = {};
+    },
 
-    apiSaveCompanyInfo() {}
+    response() {
+      if (this.isEqual(this.company, this.companyOld)) {
+        this.isEditing = false;
+        this.showSuccessToast('Inga ändringar gjorda', 1500);
+        return;
+      }
+
+      this.apiSaveCompanyInfo();
+    },
+
+    apiGetCompanyInfo() {
+      axios
+        .get('api/company')
+        .then(response => {
+          this.company = response.data;
+        })
+        .catch(error => {
+          this.showErrorToast('Något gick fel. Försök igen senare');
+        });
+    },
+
+    apiSaveCompanyInfo() {
+      axios
+        .put('api/company', this.company)
+        .then(response => {
+          this.showSuccessToast('Ändringarna sparades', 1500);
+        })
+        .catch(error => {
+          this.showErrorToast('Något gick fel. Försök igen senare');
+        })
+        .finally(() => {
+          this.isEditing = false;
+        });
+    }
   }
 };
 </script>

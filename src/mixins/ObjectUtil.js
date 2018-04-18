5a169383aba6be1b6c70bfaf374d0df9efd48715
Maciej Siwek <me@maciejsiwek.com>
2018-04-18 19:57:58 +0200

    Added object utillity class

---
+++ src/mixins/ObjectUtil.js
@@ -0,0 +1,12 @@
+export let ObjectUtil = {
+
+  methods: {
+    isEmpty(object) {
+      return Object.keys(obj).length === 0 && obj.constructor === Object;
+    },
+
+    isEqual(obj1, obj2) {
+      return JSON.stringify(obj1) === JSON.stringify(obj2)
+    }
+  }
+};

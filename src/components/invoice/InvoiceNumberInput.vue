48ff6e0b70293c37c65231d50d3945ebcb2f45f9
Maciej Siwek <me@maciejsiwek.com>
2018-04-23 00:03:05 +0200

    Invoice number

---
+++ src/components/invoice/InvoiceNumberInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="field">
+    <label class="label">Faktura Nr*</label>
+    <p class="control">
+      <input v-model="number" v-validate="'required|numeric'" @input="numberChanged()" :class="{'input is-medium has-text-centered': true, 'is-danger': errors.has('nummer')}" name="nummer" min="1" type="number" placeholder="#" autocomplete="off" autocorrect="off" spellcheck="false">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: ''
+    };
+  },
+
+  methods: {
+    numberChanged() {
+      this.$emit('changed', this.number);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.label {
+  color: #4a4a4a;
+}
+</style>
